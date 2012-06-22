733615faf7dd72729cb1dc301a6f3c15a17dc9ad
Nathan Chantrell <nathan@chantrell.net>
2012-06-22 21:20:53 +0100

    first commit

---
+++ TinyTX_DS18B20/TinyTX_DS18B20.ino
@@ -0,0 +1,95 @@
+//--------------------------------------------------------------------------------------
+// TinyTX ATtiny84 and RFM12B Wireless Temperature Sensor Node
+// By Nathan Chantrell http://nathan.chantrell.net
+//
+// Using the Dallas DS18B20 temperature sensor
+//
+// GNU GPL V3
+//--------------------------------------------------------------------------------------
+
+#include <OneWire.h> // http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip
+#include <DallasTemperature.h> // http://download.milesburton.com/Arduino/MaximTemperature/DallasTemperature_371Beta.zip
+#include <JeeLib.h> // https://github.com/jcw/jeelib
+
+ISR(WDT_vect) { Sleepy::watchdogEvent(); } // interrupt handler for JeeLabs Sleepy power saving
+
+#define myNodeID 1      // RF12 node ID in the range 1-30
+#define network 210      // RF12 Network group
+#define freq RF12_433MHZ // Frequency of RFM12B module
+
+#define ONE_WIRE_BUS 10 // DS18B20 Temperature sensor is connected on D10
+#define ONE_WIRE_POWER 9 // DS18B20 Power pin is connected on pin D9
+
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance
+
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature
+
+//########################################################################################################################
+//Data Structure to be sent
+//########################################################################################################################
+
+ typedef struct {
+  	  int temp;	// Temperature reading
+  	  int supplyV;	// Supply voltage
+ } Payload;
+
+ Payload temptx;
+
+//########################################################################################################################
+
+void setup() {
+
+  rf12_initialize(myNodeID,freq,network); // Initialize RFM12 with settings defined above 
+  rf12_sleep(0);                          // Put the RFM12 to sleep
+
+  pinMode(ONE_WIRE_POWER, OUTPUT); // set power pin for DS18B20 to output
+ 
+}
+
+void loop() {
+  
+  digitalWrite(ONE_WIRE_POWER, HIGH); // turn DS18B20 sensor on
+
+  delay(10); // Allow 10ms for the sensor to be ready
+ 
+  sensors.begin(); //start up temp sensor
+  sensors.requestTemperatures(); // Get the temperature
+  temptx.temp=(sensors.getTempCByIndex(0)*100); // Read first sensor and convert to integer, reversed at receiving end
+  
+  digitalWrite(ONE_WIRE_POWER, LOW); // turn DS18B20 off
+  
+  temptx.supplyV = readVcc(); // Get supply voltage
+
+  rfwrite(); // Send data via RF 
+
+  Sleepy::loseSomeTime(60000); //JeeLabs power save function: enter low power mode for 60 seconds (valid range 16-65000 ms)
+    
+}
+
+//--------------------------------------------------------------------------------------------------
+// Send payload data via RF
+//--------------------------------------------------------------------------------------------------
+ static void rfwrite(){
+   rf12_sleep(-1);     //wake up RF module
+   while (!rf12_canSend())
+   rf12_recvDone();
+   rf12_sendStart(0, &temptx, sizeof temptx); 
+   rf12_sendWait(2);    //wait for RF to finish sending while in standby mode
+   rf12_sleep(0);    //put RF module to sleep
+}
+
+//--------------------------------------------------------------------------------------------------
+// Read current supply voltage
+//--------------------------------------------------------------------------------------------------
+ long readVcc() {
+   long result;
+   // Read 1.1V reference against Vcc
+   ADMUX = _BV(MUX5) | _BV(MUX0);
+   delay(2); // Wait for Vref to settle
+   ADCSRA |= _BV(ADSC); // Convert
+   while (bit_is_set(ADCSRA,ADSC));
+   result = ADCL;
+   result |= ADCH<<8;
+   result = 1126400L / result; // Back-calculate Vcc in mV
+   return result;
+}
