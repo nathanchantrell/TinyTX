83984f9ec6ea79e9d09fa590e473fb6970a1b71e
Nathan Chantrell <nathan@chantrell.net>
2012-06-25 21:20:20 +0100

    add DHT22 version

--- TinyTX_DHT22/TinyTX_DHT22.ino
+++ TinyTX_DHT22/TinyTX_DHT22.ino
@@ -1,40 +1,37 @@
 //----------------------------------------------------------------------------------------------------------------------
-// TinyTX - An ATtiny84 and RFM12B Wireless Temperature Sensor Node
+// TinyTX - An ATtiny84 and RFM12B Wireless Temperature & Humidity Sensor Node
 // By Nathan Chantrell. For hardware design see http://nathan.chantrell.net/tinytx
 //
-// Using the Dallas DS18B20 temperature sensor
+// Using the DHT22 temperature and humidity sensor
 //
 // Licenced under the Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) licence:
 // http://creativecommons.org/licenses/by-sa/3.0/
 //
 // Requires Arduino IDE with arduino-tiny core: http://code.google.com/p/arduino-tiny/
-// and small change to OneWire library, see: http://arduino.cc/forum/index.php/topic,91491.msg687523.html#msg687523
 //----------------------------------------------------------------------------------------------------------------------
 
-#include <OneWire.h> // http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip
-#include <DallasTemperature.h> // http://download.milesburton.com/Arduino/MaximTemperature/DallasTemperature_371Beta.zip
+#include <DHT22.h> // https://github.com/ringerc/Arduino-DHT22
 #include <JeeLib.h> // https://github.com/jcw/jeelib
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // interrupt handler for JeeLabs Sleepy power saving
 
-#define myNodeID 1      // RF12 node ID in the range 1-30
+#define myNodeID 16      // RF12 node ID in the range 1-30
 #define network 210      // RF12 Network group
 #define freq RF12_433MHZ // Frequency of RFM12B module
 
-#define ONE_WIRE_BUS 10 // DS18B20 Temperature sensor is connected on D10/ATtiny pin 13
-#define ONE_WIRE_POWER 9 // DS18B20 Power pin is connected on pin D9/ATtiny pin 12
+#define DHT22_PIN 10     // DHT sensor is connected on D10/ATtiny pin 13
+#define DHT22_POWER 9 // DHT Power pin is connected on pin D9/ATtiny pin 12
 
-OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance
-
-DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature
+DHT22 myDHT22(DHT22_PIN); // Setup the DHT
 
 //########################################################################################################################
 //Data Structure to be sent
 //########################################################################################################################
 
  typedef struct {
-  	  int temp;	// Temperature reading
+   	  int temp;	// Temperature reading
   	  int supplyV;	// Supply voltage
+     	  int humidity;	// Humidity reading
  } Payload;
 
  Payload tinytx;
@@ -46,26 +43,34 @@ void setup() {
   rf12_initialize(myNodeID,freq,network); // Initialize RFM12 with settings defined above 
   rf12_sleep(0);                          // Put the RFM12 to sleep
 
-  pinMode(ONE_WIRE_POWER, OUTPUT); // set power pin for DS18B20 to output
- 
+  pinMode(DHT22_POWER, OUTPUT); // set power pin for DHT to output
+  
 }
 
 void loop() {
   
-  digitalWrite(ONE_WIRE_POWER, HIGH); // turn DS18B20 sensor on
+  digitalWrite(DHT22_POWER, HIGH); // turn DHT sensor on
 
-  delay(10); // Allow 10ms for the sensor to be ready
- 
-  sensors.begin(); //start up temp sensor
-  sensors.requestTemperatures(); // Get the temperature
-  tinytx.temp=(sensors.getTempCByIndex(0)*100); // Read first sensor and convert to integer, reversed at receiving end
+  DHT22_ERROR_t errorCode;
   
-  digitalWrite(ONE_WIRE_POWER, LOW); // turn DS18B20 off
+  delay(2000); // Sensor requires minimum 2s warm-up after power-on.
   
-  tinytx.supplyV = readVcc(); // Get supply voltage
+  errorCode = myDHT22.readData(); // read data from sensor
 
-  rfwrite(); // Send data via RF 
+  if (errorCode == DHT_ERROR_NONE) { // data is good
 
+    tinytx.temp = (myDHT22.getTemperatureC()*100); // Get temperature reading and convert to integer, reversed at receiving end
+    
+    tinytx.humidity = (myDHT22.getHumidity()*100); // Get humidity reading and convert to integer, reversed at receiving end
+
+    tinytx.supplyV = readVcc(); // Get supply voltage
+
+    rfwrite(); // Send data via RF 
+
+  }
+
+  digitalWrite(DHT22_POWER, LOW); // turn DS18B20 off
+  
   Sleepy::loseSomeTime(60000); //JeeLabs power save function: enter low power mode for 60 seconds (valid range 16-65000 ms)
     
 }
